b258d177042b9d5b77e04974b00d0eaee92cf7af
Kedanco <kelvinchowjunjie120@hotmail.com>
2017-11-05 12:37:26 +0800

    incompleted query

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -21,10 +21,67 @@ class PagesController < ApplicationController
   def output_to_map
     @districts = District.all
 
+    search_value = params[:search_result]
     area_value = params[:area_checkbox]
-    search_result = params[:search_result]
-    crowdlevel = params[:crowdlevel]
-    datetime = params[:date_time]
+    crowdlevel_value = params[:crowdlevel]
+    datetime_value = params[:date_time]
+
+    crowd_filtered_places = []
+
+    # Get search results
+
+    search_places = Place.where("name LIKE %search_value%")
+
+    # Implement filters on search_places
+
+    # Area filter
+    if area_value != nil
+
+      search_places.delete_if{|place| !area_value.include?(place.district.area_id)}
+
+    end
+
+    # Date & Time Filter
+    if datetime_value != nil
+
+      search_places.delete_if{ |place|
+
+        hour = datetime_value.strftime("%H")
+        day = datetime_value.strftime("%A")
+
+        place.each do |crowd_level|
+
+           (crowd_level.hour != hour) || (crowd_level.day != day)
+
+        end
+
+      }
+
+    end
+
+    # Crowded/Not Crowded Filter
+    if crowdlevel_value != nil
+
+      search_places.delete_if{|place|
+
+        cl = place.crowd_levels
+        average_density = (cl.sum(:crowd_density)/cl.count)
+
+          if crowdlevel_value == crowded
+
+            crowd_level.crowd_density > 75
+
+          elsif crowdlevel_value == not_crowded
+
+            crowd_level.crowd_density < 25
+
+          end
+
+        end
+
+      }
+
+    end
 
     @areas = Area.where(name: area_value)  
     # render "pages/home"  
